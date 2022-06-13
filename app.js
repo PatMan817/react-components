f5a48fec6c5c95d91c54dc14bc8cff05abededac
Patrick O'Shea <osheap81796@gmail.com>
2022-06-13 12:42:47 -0400

    Completed solo part

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      have: false
+    };
+  }
+
+  onLIClick() {
+    this.setState({
+      have: !this.state.have
+    });
+  }
+
+  render() {
+
+    var style = {
+      textDecoration: this.state.have ? 'line-through' : 'none'
+    };
+
+    return (
+    <li style={style} onClick={this.onLIClick.bind(this)}>{this.props.items}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(items =>
+    <GroceryListItem items={items} />
+  )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList items={['Bread', 'Milk']}/>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'));
